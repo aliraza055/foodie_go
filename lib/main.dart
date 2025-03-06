0d3bb1399ced23c8b8608df02fb4b8a13b2db6f5
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-03-06 19:38:52 +0500

    edit theme

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(    
-      ),
+      
       home: const HomePage()
     );
   }
