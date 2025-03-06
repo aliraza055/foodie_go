5523e17a1186b91dd877a74f44a9571ff25efc36
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-03-06 19:24:16 +0500

    set up firebase

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,21 +1,37 @@
-allprojects {
+buildscript {
+    val kotlinVersion = "1.9.23" // Use a stable Kotlin version
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.1") // Use 8.2.1 (stable)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+allprojects { 
     repositories {
         google()
         mavenCentral()
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.buildDir = file("../build")
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
+project.buildDir = file("${rootProject.buildDir}/${project.name}")}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+    delete(layout.buildDirectory)
 }
+
+
+
+
+
--- lib/pages/detail.dart
+++ lib/pages/detail.dart
@@ -13,20 +13,81 @@ class DeatailPage extends StatefulWidget {
 }
 
 class _DeatailPageState extends State<DeatailPage> {
+  int count=1;
+  int basePrice=0;
+  @override
+  void initState() {
+    basePrice=int.parse(widget.price);
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(top: 40,left: 10),
+        margin: EdgeInsets.only(top: 40,left: 20),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Icon(Icons.arrow_back),
+            GestureDetector(
+              onTap: ()=>Navigator.pop(context),
+              child: Container(
+                height: 30,
+                width:30 ,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(30)
+                ),
+                child: Icon(Icons.arrow_back,color: Colors.white,))),
             SizedBox(height: 30,),
-            Image.asset(widget.image,height: 200,width: 200,fit: BoxFit.contain,),
+            Center(child: Image.asset(widget.image,height: MediaQuery.of(context).size.height/3,fit: BoxFit.contain,)),
             SizedBox(height: 10,),
-            Text(widget.name,style: AppServices().whiteTextStyle(),),
+            Text(widget.name,style: AppServices().mainHeadingStyle(),),
             SizedBox(height: 10,),
-            Text(widget.price,style: AppServices().whiteTextStyle(),)
+            Text("Rs:${widget.price}",style: AppServices().mainHeadingStyle(),),
+            SizedBox(height: 60,),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: (){
+                    setState(() {
+                      count++;
+                      basePrice+=int.parse(widget.price);
+                    });
+                  },
+                  child: Container(
+                    height: 30,
+                    width: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.red
+                    ),
+                    child: Center(child: Icon(Icons.add,color: Colors.white,)),),
+                ),
+                  SizedBox(width: 10,),
+                  Text('$count',style: TextStyle(fontSize: 30),),
+                  SizedBox(width: 10,),
+                   GestureDetector(
+                    onTap: (){
+                      if(count>1){
+                        setState(() {
+                           count--;
+                        basePrice-=int.parse(widget.price);                    
+                        });
+                      }
+                    },
+                     child: Container(
+                      padding: EdgeInsets.only(bottom: 15),
+                                       height: 30,
+                                       width: 30,
+                                       decoration: BoxDecoration(
+                      color: Colors.red
+                                       ),
+                                       child: Icon(Icons.minimize,color: Colors.white,),),
+                   ),
+              ],
+            ),
+            SizedBox(height: 20,),
+            Text("Rs:$basePrice",style:AppServices().simpleTextStyle() ,)
           ],
         ),
       ),
