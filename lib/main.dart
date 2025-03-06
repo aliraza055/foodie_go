c8d1e36dd8b67e690106989662dd96bb033fe6aa
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-03-06 19:38:16 +0500

    remove title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter ',
       theme: ThemeData(    
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
