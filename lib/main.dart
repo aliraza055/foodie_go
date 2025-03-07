eeac0617bedd62655a9e5ccf946b8579a7ec4d76
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-03-07 23:12:34 +0500

    aligning

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:foodie_go/pages/homepage.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
