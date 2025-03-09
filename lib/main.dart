648abf51cbc46ee23742c2135112480c113f55cd
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-03-09 16:44:05 +0500

    create singup page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:foodie_go/pages/homepage.dart';
+import 'package:foodie_go/pages/singup.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       
-      home: const HomePage()
+      home: const Singup()
     );
   }
 }
---
+++ lib/pages/singup.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Singup extends StatefulWidget {
+  const 
+Singup({super.key});
+
+  @override
+  State<
+Singup> createState() => _SingupState();
+}
+
+class _SingupState extends State<Singup> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Container(
+        child: Column(
+          
+        ),
+      ),
+    );
+  }
+}
