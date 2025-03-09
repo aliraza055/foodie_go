acdc27380925e6248d03927507569b43c67f534f
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-03-09 21:36:22 +0500

    change color

--- lib/pages/singup.dart
+++ lib/pages/singup.dart
@@ -14,8 +14,37 @@ class _SingupState extends State<Singup> {
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Container(
-        child: Column(
-          
+        child: Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height:MediaQuery.of(context).size.height/3,
+              decoration: BoxDecoration(
+                color: Color(0xffffefbf),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
+                  )
+              ),
+              child: Column(
+                children: [
+                  Center(child: Image.asset('images/logo2.png',height: 100,width: 100,fit: BoxFit.contain,)),
+                 Center(
+                  child: Image.asset('images/logo2.png',height: 100,width: 100,fit: BoxFit.contain,)),
+
+                ],
+              ),
+            ),
+            Positioned(
+              right: 100,
+              top: 500,
+              child: Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+              ),
+            )
+          ],
         ),
       ),
     );
