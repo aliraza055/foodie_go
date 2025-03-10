7bcaf4a047e13d90ef782d16083d7bdaa11beffd
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-03-10 23:37:07 +0500

    make singupo make

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       
-      home: const Singup()
+      home: const Signup()
     );
   }
 }
--- lib/pages/singup.dart
+++ lib/pages/singup.dart
@@ -1,50 +1,130 @@
 import 'package:flutter/material.dart';
 
-class Singup extends StatefulWidget {
-  const 
-Singup({super.key});
+class Signup extends StatefulWidget {
+  const Signup({super.key});
 
   @override
-  State<
-Singup> createState() => _SingupState();
+  State<Signup> createState() => _SignupState();
 }
 
-class _SingupState extends State<Singup> {
+class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Container(
-        child: Stack(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height:MediaQuery.of(context).size.height/3,
-              decoration: BoxDecoration(
-                color: Color(0xffffefbf),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(50),
-                  bottomRight: Radius.circular(50),
-                  )
-              ),
-              child: Column(
-                children: [
-                  Center(child: Image.asset('images/logo2.png',height: 100,width: 100,fit: BoxFit.contain,)),
-                 Center(
-                  child: Image.asset('images/logo2.png',height: 100,width: 100,fit: BoxFit.contain,)),
-
-                ],
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height / 3,
+            decoration: const BoxDecoration(
+              color: Color(0xffffefbf),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50),
               ),
             ),
-            Positioned(
-              right: 100,
-              top: 500,
-              child: Container(
+            child: Center(
+              child: Image.asset(
+                'images/logo2.png',
                 height: 100,
                 width: 100,
-                color: Colors.red,
+                fit: BoxFit.contain,
               ),
-            )
-          ],
+            ),
+          ),      
+          
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            margin: EdgeInsets.only(top: 200,left: 20,right: 20,bottom: 50),
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 5,
+                  blurRadius: 10,
+                  offset: const Offset(0, 5),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  "Sign Up",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                const SizedBox(height: 40),
+      
+                // Name Field
+                _buildTextField(
+                  hintText: "Enter your name",
+                  icon: Icons.person,
+                ),
+      
+                // Email Field
+                const SizedBox(height: 20),
+                _buildTextField(
+                  hintText: "Enter your email",
+                  icon: Icons.email,
+                ),
+      
+                // Password Field
+                const SizedBox(height: 20),
+                _buildTextField(
+                  hintText: "Enter your password",
+                  icon: Icons.lock,
+                  obscureText: true,
+                ),
+      
+                // Sign Up Button
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xffffefbf),
+                    padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    "Sign Up",
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),     
+        ],
+      ),
+    );
+  }
+
+  // Custom method to create text fields
+  Widget _buildTextField({required String hintText, required IconData icon, bool obscureText = false}) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: TextFormField(
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          hintText: hintText,
+          border: InputBorder.none,
+          prefixIcon: Icon(icon, color: Colors.black54),
+          filled: true,
+          fillColor: Colors.transparent,
         ),
       ),
     );
