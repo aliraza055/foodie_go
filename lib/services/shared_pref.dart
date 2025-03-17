909259bcfd8cd8a079cd7ce7153f60f50177bc2b
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-03-17 22:32:53 +0500

    create shared preference

--- lib/services/shared_pref.dart
+++ lib/services/shared_pref.dart
@@ -6,6 +6,28 @@ class SharedPrefercesHelper{
   static String gmailIDkey='GMAILKEY';
   setUserID(String id)async{
     SharedPreferences pref=await SharedPreferences.getInstance();
-    pref.setString(userIdKey,id );
+  return  pref.setString(userIdKey,id );
+  }
+   getUserID()async{
+    SharedPreferences pref=await SharedPreferences.getInstance();
+   return pref.getString(userIdKey);
+  }
+  setUserName(String name)async{
+    SharedPreferences pref=await SharedPreferences.getInstance();
+    pref.setString(nameIdKey, name);
+  }
+  getUserName()async{
+ SharedPreferences pref=await SharedPreferences.getInstance();
+    return pref.getString(nameIdKey);
+
+  }
+    setUserGmail(String gmail)async{
+    SharedPreferences pref=await SharedPreferences.getInstance();
+  return  pref.setString(gmailIDkey,gmail );
+  }
+    getUserGmail()async{
+ SharedPreferences pref=await SharedPreferences.getInstance();
+    return pref.getString(gmailIDkey);
+
   }
 }
