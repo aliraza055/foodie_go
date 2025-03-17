84d45234c4e6cf70dc41ef63300feb53faa372a2
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-03-17 22:21:10 +0500

    ser user prefences

--- lib/services/shared_pref.dart
+++ lib/services/shared_pref.dart
@@ -1,3 +1,11 @@
-class SharedPreferces{
-  
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefercesHelper{
+  static String userIdKey='USERKEY';
+  static String nameIdKey='NAMEKEY';
+  static String gmailIDkey='GMAILKEY';
+  setUserID(String id)async{
+    SharedPreferences pref=await SharedPreferences.getInstance();
+    pref.setString(userIdKey,id );
+  }
 }
