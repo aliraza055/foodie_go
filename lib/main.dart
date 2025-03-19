8ab8d8b7fcc9d15d8933d031f8fec3dbd8766dac
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-03-19 20:02:22 +0500

    intergating flutter

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       
-      home: const OnBoaridng ()
+      home: const  ()
     );
   }
 }
