dcd754c081e141ca9131f9a30bdd635d4a414024
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-03-19 20:01:56 +0500

    background

--- lib/services/toast.dart
+++ lib/services/toast.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 class ToastError{
-  void toast( String message,Color bColor){
+  void toast( String message,Color ){
     Fluttertoast.showToast(
       msg: message,
       toastLength: Toast.LENGTH_LONG,
