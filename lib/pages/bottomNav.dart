6d80e3a7cd3801e7a9dbb360524e45f10dbf03d8
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-04-06 21:38:42 +0500

    create bottom navigation bar

---
+++ lib/pages/bottomNav.dart
@@ -0,0 +1,56 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:foodie_go/pages/homepage.dart';
+import 'package:foodie_go/pages/order.dart';
+import 'package:foodie_go/pages/profile.dart';
+import 'package:foodie_go/pages/wallet.dart';
+
+class BottomNavigation extends StatefulWidget {
+
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+ late List<Widget> pages;
+  late HomePage _homePage;
+  late Order _order;
+ late  Wallet _wallet;
+ late Profile _profile;
+ int currentIndex=0;
+ @override
+  void initState() {
+    _homePage=HomePage();
+    _order=Order();
+    _wallet=Wallet();
+    _profile=Profile();
+    pages=[_homePage,_order,_wallet,_profile];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar:CurvedNavigationBar(
+        backgroundColor: Colors.black,
+        height: 70,
+        animationDuration: Duration(milliseconds: 500),
+        onTap: (value) {
+          setState(() {
+            currentIndex=value;
+          });
+        },
+        items: [
+        Icon(Icons.home,color: Colors.white,size: 30,),
+        Icon(Icons.shopping_bag,color: Colors.white,size: 30,),
+        Icon(Icons.wallet,color: Colors.white,size: 30,),
+        Icon(Icons.person,color: Colors.white,size: 30,)
+
+      ],
+      ),
+      body: pages[currentIndex],
+    );
+  }
+}
