887e21cdcc02453bb284a49ba40ca0a601734f94
Ali Raza Maqbool <alirazamaqbool055@gmail.com>
2025-04-25 22:19:12 +0500

    creating share preference

--- lib/pages/order.dart
+++ lib/pages/order.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodie_go/services/widget_serves.dart';
 
 class Order extends StatefulWidget {
   const Order({super.key});
@@ -11,10 +12,43 @@ class _OrderState extends State<Order> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-         
-        ],
+      body: Container(
+        margin: EdgeInsets.only(top: 30),
+        child: Column(
+          children: [
+           Row(
+            mainAxisAlignment: MainAxisAlignment.center
+            ,
+            children: [
+              Text("Orders",style: AppServices().mainHeadingStyle(),)
+            ],
+           ),
+           Expanded(
+             child: Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Color(0xffececf8),
+                borderRadius: BorderRadius.only(topRight:Radius.circular(30),topLeft: Radius.circular(30)),
+             
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 20,left: 20,right: 20),
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.white,
+                    child: Row(
+                      children: [
+                        Image.asset('images/pizza.png',height: 150,width: 150,)
+                      ],
+                    ),
+                  )
+                ],
+              ),
+             ),
+           )
+          ],
+        ),
       ),
     );
   }
--- lib/services/shared_pref.dart
+++ lib/services/shared_pref.dart
@@ -4,6 +4,10 @@ class SharedPrefercesHelper{
   static String userIdKey='USERKEY';
   static String nameIdKey='NAMEKEY';
   static String gmailIDkey='GMAILKEY';
+    static String adressIDkey='ADRESSKEY';
+
+
+
   setUserID(String id)async{
     SharedPreferences pref=await SharedPreferences.getInstance();
   return  pref.setString(userIdKey,id );
